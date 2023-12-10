3d662f5e68cf6efc34e5aa55fc5824e6a0975860
Khusnasin <khusnasinsultana@gmail.com>
2023-12-10 17:53:39 +0530

    commit-3

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar';
+import RegisterUser from './Screens/RegisterUser';
+import UserLogin from './Screens/UserLogin';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-       
-      
+      <Navbar />
+
+      <Router>
+        <Routes>
+          <Route path='/registeruser' element={<RegisterUser />} />
+          <Route path='/loginuser' element={<UserLogin />} />
+        </Routes>
+      </Router>
+
     </div>
   );
 }
